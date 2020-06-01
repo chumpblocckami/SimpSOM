3939c20e840bf16f197b542af7d05e081e4f3798
Matteo Mazzola <mazzolauni@gmail.com>
2020-06-01 15:21:57 +0200

    finally updated the cmap

--- SimpSOM/hexagons.py
+++ SimpSOM/hexagons.py
@@ -31,7 +31,7 @@ def coorToHex(x,y):
     return [newx,newy]    
     
 
-def plot_hex(fig, centers, weights):
+def plot_hex(fig, centers, weights,cmap_type="Greys"):
     
     """Plot an hexagonal grid based on the nodes positions and color the tiles
        according to their weights.
@@ -53,30 +53,27 @@ def plot_hex(fig, centers, weights):
     xpoints = [x[0]  for x in centers]
     ypoints = [x[1]  for x in centers]
     patches = []
-    #is istance means that the first element is the type of the second one
+
     if any(isinstance(el, list) for el in weights) and len(weights[0])==3:
-        print("SI")
-        cmap = plt.get_cmap('Greys')
         for x,y,w in zip(xpoints,ypoints,weights):
             hexagon = RegularPolygon((x,y), numVertices=6, radius=.95/np.sqrt(3), 
                                 orientation=np.radians(0), 
-                                facecolor=cmap(w))
+                                facecolor=w)
             ax.add_patch(hexagon)
 
     else:
-            
-        cmap = plt.get_cmap('Greys')
+        cmap = plt.cm.get_cmap(cmap_type)
         for x,y,w in zip(xpoints,ypoints,weights):
             hexagon = RegularPolygon((x,y), numVertices=6, radius=.95/np.sqrt(3), 
                                 orientation=np.radians(0), 
                                 facecolor=cmap(w))
             patches.append(hexagon) 
 
-        p = PatchCollection(patches)
+        p = PatchCollection(patches,cmap=cmap)
         p.set_array(np.array(weights))
         ax.add_collection(p)
         
     ax.axis('off')
     ax.autoscale_view()
-    print("FINITO!")
+    
     return ax
